ebb02508cd8fcf638cd1b4849a089a12b3c87ea1
Loay Ghreeb <Loayahmed655@gmail.com>
2025-04-17 00:18:16 +0200

    Use local postgres

--- README.md
+++ README.md
@@ -1,33 +1,34 @@
 # UserAPI
 
 UML:
+
 ```mermaid
 classDiagram
     class UserAPI {
-      +addUser(user: User)
-      +getUser(id: int): User
-      +activateUser(id: int)
-      +deactivateUser(id: int)
-      +viewUsers(): List~User~
-      +deleteUser(id: int)
+        +addUser(user: User)
+        +getUser(id: int): User
+        +activateUser(id: int)
+        +deactivateUser(id: int)
+        +viewUsers(): List~User~
+        +deleteUser(id: int)
     }
-    
+
     class User {
-      -userId: int
-      -username: string
-      -email: string
-      -password: string
-      -status: boolean
-      -createdAt: datetime
-      -updatedAt: datetime
-      -lastLogin: datetime
+        -userId: int
+        -username: string
+        -email: string
+        -password: string
+        -status: boolean
+        -createdAt: datetime
+        -updatedAt: datetime
+        -lastLogin: datetime
     }
-    
-    UserAPI --> User : manages
+
+    UserAPI --> User: manages
 ```
 
-
 ERD:
+
 ```mermaid
 erDiagram
     USER {
--- src/main/java/com/fawry/user/rest/UserRestController.java
+++ src/main/java/com/fawry/user/rest/UserRestController.java
@@ -3,38 +3,45 @@ package com.fawry.user.rest;
 import com.fawry.user.entity.Users;
 import com.fawry.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/users")
 public class UserRestController {
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserRestController(UserService theUserService) {
         userService = theUserService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<Users> getUsers() {
         return userService.getUsers();
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public Users getUser(@PathVariable int userId) {
         return userService.getUser(userId);
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public Users addUser(@RequestBody Users user) {
         user.setId(0);
         userService.saveUser(user);
         return user;
     }
 
-    @PutMapping("/users")
+    @PutMapping
     public Users updateUser(@RequestBody Users user) {
         return userService.saveUser(user);
     }
@@ -45,17 +52,17 @@ public class UserRestController {
         return userService.saveUser(user);
     }
 
-    @PutMapping("/users/{userId}/activate")
+    @PutMapping("/{userId}/activate")
     public void activateUser(@PathVariable int userId) {
         userService.activateUser(userId);
     }
 
-    @PutMapping("/users/{userId}/deactivate")
+    @PutMapping("/{userId}/deactivate")
     public void deactivateUser(@PathVariable int userId) {
         userService.deactivateUser(userId);
     }
 
-    @DeleteMapping("/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable int userId) {
         userService.deleteUser(userId);
         return "Deleted user id - " + userId;
